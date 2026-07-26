ba36abde3f6a7f862d36fe5b818bcd8532bdc642
MG <morokiane@gmail.com>
2026-07-25 19:26:25 -0600

    quatrieme jour

--- Scripts/Controllers/LevelController.gd
+++ Scripts/Controllers/LevelController.gd
@@ -88,3 +88,7 @@ func IsMaxed(ability: String) -> bool:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemy"):
 		area.get_parent().queue_free()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/Castle.scn")
